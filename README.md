94cca5eb7d91fb73dcc80454c370fc5047b73b97
Matthew Howle <mdhowle@gmail.com>
2019-07-20 13:40:36 -0400

    Change to unittest and add DEB archive tests

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ arlib
 
 A Python library for reading and writing "ar" archives.
 
-[![Build Status](https://travis-ci.org/awm/arlib.svg?branch=master)](https://travis-ci.org/awm/arlib)
+[![Build Status](https://travis-ci.org/mdhowle/arlib.svg?branch=master)](https://travis-ci.org/mdhowle/arlib)
--- tests.py
+++ tests.py
@@ -1,76 +1,105 @@
-import os
-import nose
-import shutil
 import logging
+import os
+import shutil
 import tempfile
+import unittest
 
 import arlib
 
 logging.getLogger("arlib").setLevel(logging.DEBUG)
 
-temp_dir = None
+class ArLibTestCase(unittest.TestCase):
+    def setUp(self):
+        self.temp_dir = tempfile.mkdtemp()
 
-def setup():
-    global temp_dir
-    temp_dir = tempfile.mkdtemp()
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir, ignore_errors=True)
+        self.temp_dir = None
 
-def teardown():
-    global temp_dir
-    shutil.rmtree(temp_dir, ignore_errors=True)
+    def test_loading_bsd_archive(self):
+        arc = arlib.Archive()
+        arc.load("test_subjects/bsd1.a")
+        self.assertEqual(arc.format, arlib.BSD)
+        self.assertNotEqual(arc.symbols, None)
+        self.assertEqual(len(arc), 5)
 
-@nose.with_setup(setup, teardown)
-def test_loading_bsd_archive():
-    global temp_dir
+        filenames = sorted([m.filename for m in arc])
+        expected = ["alpha.o", "another_long_file_name.o", "test.o", "this_is_a_long_file_name.o", "zeta.o"]
+        self.assertEqual(expected, filenames)
+        self.assertEqual(arc["test.o"].mode, 0o100644)
 
-    arc = arlib.Archive()
-    arc.load("test_subjects/bsd1.a")
-    assert arc.format == arlib.BSD
-    assert arc.symbols is not None
-    assert len(arc) == 5
+        arc.extract_all(self.temp_dir)
+        for f in expected:
+            s = os.stat(os.path.join(self.temp_dir, f))
+            self.assertEqual(s.st_mode, arc[f].mode)
+            self.assertEqual(s.st_size, arc[f].filesize)
+            self.assertEqual(s.st_mtime, arc[f].date) 
 
-    filenames = sorted([m.filename for m in arc])
-    expected = ["alpha.o", "another_long_file_name.o", "test.o", "this_is_a_long_file_name.o", "zeta.o"]
-    assert expected == filenames
-    assert arc["test.o"].mode == 0o100644
+    def test_loading_gnu_archive(self):
+        arc = arlib.Archive()
+        arc.load("test_subjects/gnu1.a")
+        self.assertEqual(arc.format, arlib.GNU)
+        self.assertNotEqual(arc.symbols, None)
+        self.assertNotEqual(arc.strings, None)
+        self.assertEqual(len(arc), 5)
 
-    arc.extract_all(temp_dir)
-    for f in expected:
-        s = os.stat(os.path.join(temp_dir, f))
-        assert s.st_mode == arc[f].mode
-        assert s.st_size == arc[f].filesize
-        assert s.st_mtime == arc[f].date
+        filenames = sorted([m.filename for m in arc])
+        expected = ["alpha.o", "another_long_file_name.o", "test.o", "this_is_a_long_file_name.o", "zeta.o"]
+        self.assertEqual(expected, filenames)
+        self.assertEqual(arc["test.o"].mode,  0o100644)
 
-@nose.with_setup(setup, teardown)
-def test_loading_gnu_archive():
-    global temp_dir
+        arc.extract_all(self.temp_dir)
+        for f in expected:
+            s = os.stat(os.path.join(self.temp_dir, f))
+            self.assertEqual(s.st_mode, arc[f].mode)
+            self.assertEqual(s.st_size, arc[f].filesize)
+            self.assertEqual(s.st_mtime, arc[f].date)
 
-    arc = arlib.Archive()
-    arc.load("test_subjects/gnu1.a")
-    assert arc.format == arlib.GNU
-    assert arc.symbols is not None
-    assert arc.strings is not None
-    assert len(arc) == 5
+    def test_loading_deb_archive(self):
+        arc = arlib.Archive()
+        arc.load("test_subjects/test.deb")
+        self.assertEqual(arc.format, arlib.DEB)
+        self.assertEqual(arc.symbols, None)
+        self.assertEqual(len(arc), 3)
 
-    filenames = sorted([m.filename for m in arc])
-    expected = ["alpha.o", "another_long_file_name.o", "test.o", "this_is_a_long_file_name.o", "zeta.o"]
-    assert expected == filenames
-    assert arc["test.o"].mode == 0o100644
+        filenames = [m.filename for m in arc]
+        expected = ["debian-binary", "control.tar.xz", "data.tar.xz"]
+        self.assertEqual(expected, filenames)
+        self.assertEqual(arc["debian-binary"].mode, 0o100644)
 
-    arc.extract_all(temp_dir)
-    for f in expected:
-        s = os.stat(os.path.join(temp_dir, f))
-        assert s.st_mode == arc[f].mode
-        assert s.st_size == arc[f].filesize
-        assert s.st_mtime == arc[f].date
+        arc.extract_all(self.temp_dir)
+        for f in expected:
+            s = os.stat(os.path.join(self.temp_dir, f))
+            self.assertEqual(s.st_mode, arc[f].mode)
+            self.assertEqual(s.st_size, arc[f].filesize)
+            self.assertEqual(s.st_mtime, arc[f].date)
 
-@nose.with_setup(setup, teardown)
-def test_creating_bsd_archive():
-    global temp_dir
+    def test_creating_bsd_archive(self):
+        c = arlib.Archive(format=arlib.BSD)
+        c.add("test_subjects/source/alpha.c")
+        c.add("test_subjects/source/another_long_file_name.c")
+        c.add("test_subjects/source/test.c")
+        c.add("test_subjects/source/this_is_a_long_file_name.c")
+        c.add("test_subjects/source/zeta.c")
+        c.save(os.path.join(self.temp_dir, "bsd.a"))
 
-    c = arlib.Archive(format=arlib.BSD)
-    c.add("test_subjects/source/alpha.c")
-    c.add("test_subjects/source/another_long_file_name.c")
-    c.add("test_subjects/source/test.c")
-    c.add("test_subjects/source/this_is_a_long_file_name.c")
-    c.add("test_subjects/source/zeta.c")
-    c.save(os.path.join(temp_dir, "bsd.a"))
+    def test_creating_deb_archive(self):
+        c = arlib.Archive(format=arlib.DEB)
+
+        # Place in incorrect order
+        c.add("test_subjects/deb_source/control.tar.xz")
+        c.add("test_subjects/deb_source/data.tar.xz")
+        c.add("test_subjects/deb_source/debian-binary")
+        c.save(os.path.join(self.temp_dir, "test.deb"))
+        c.outstream.close()
+
+        d = arlib.Archive(format=arlib.DEB)
+        d.load(os.path.join(self.temp_dir, "test.deb"))
+
+        filenames = [m.name for m in d.members]
+        expected = ["debian-binary", "control.tar.xz", "data.tar.xz"]
+        self.assertEqual(filenames, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
